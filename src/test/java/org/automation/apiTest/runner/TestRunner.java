8f9d5ba42f2643fe1b71d475152a5aca09c16ad5
berkertan <berk_ertn@outlook.com>
2024-11-17 02:17:49 +0300

    parallel and single thread run enabled (xml selection now is TODO)

--- src/test/java/org/automation/apiTest/runner/TestRunner.java
+++ src/test/java/org/automation/apiTest/runner/TestRunner.java
@@ -4,6 +4,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 import org.testng.annotations.DataProvider;
 
+import static org.automation.apiTest.utils.LoggerUtil.logInfo;
+
 @CucumberOptions(
         features = "features",
         glue = {"org.automation.apiTest.steps", "org.automation.apiTest.utils", "org.automation.apiTest.configurations"},
@@ -17,6 +19,22 @@ public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
     @DataProvider(parallel = true)
     public Object[][] scenarios() {
+        boolean isParallel = Boolean.parseBoolean(System.getProperty("parallel.mode", "false"));
+        setParallelMode(isParallel);
+        if (!isParallel) {
+            logInfo("Overriding parallel execution to single-thread mode.");
+            System.setProperty("dataproviderthreadcount", "1");
+        } else {
+            System.setProperty("dataproviderthreadcount", "4");
+        }
         return super.scenarios();
+    }
+
+    private void setParallelMode(boolean isParallel) {
+        if (isParallel) {
+            logInfo("Running tests in parallel mode.");
+        } else {
+            logInfo("Running tests in single-thread mode.");
+        }
     }
 }
